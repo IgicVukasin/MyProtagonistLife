5ef010c1e737bbcc287ba4b3f1deb0bf2140ad09
Vukasin <vulecar@live.com>
2019-07-01 01:05:47 +0200

    init

---
+++ app/Http/Controllers/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Test extends Controller
+{
+    protected function test()
+    {
+        return 'radi';
+    }
+}
